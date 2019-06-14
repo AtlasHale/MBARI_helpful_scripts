8a75f9829864a5d815d18d4de534b885cb315be1
Connor Hale <37227138+AtlasHale@users.noreply.github.com>
2019-06-14 10:41:47 -0700

    added species histogram to show rare species

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 def main():
     """
@@ -7,40 +8,42 @@ def main():
     and create histogram from specific tapes to find high density 
     areas of tape to improve tape to digital conversion efficiency.
     """
-    file = open(input('Enter tab separated metadata file name: '))
+    file = open('rare_benthic_filter')
     all_lines = []
     for line in file:
         fields = line.split('\t')
+        # remove trailing newlines, spaces replaced with _
         for i in range(len(fields)):
             single = fields[i]
             temp_single = ""
             for j in range(len(single)):
                 if single[j] == ' ':
                     if j != 0 and j != len(single)-1:
-                        temp_single+='_'
+                        temp_single += '_'
                 elif single[j] == '\n':
                     continue
                 else:
-                    temp_single+=single[j]
+                    temp_single += single[j]
             fields[i] = temp_single
             if fields[i] == '':
                 fields.pop(i)
         all_lines.append(fields)
 
-    occurences = species_per_tape(all_lines)
-    print_tapes(occurences)
-    prompt_tape(all_lines)
+    # occurences = species_per_tape(all_lines)
+    # print_tapes(occurences)
+    species_split_histogram(all_lines, input("Video ID: "))
+    # specific_species(all_lines)
+    # prompt_tape(all_lines)
     start_and_end()
 
-
-def uniqueFilter(all_lines):
-    #for single_line in all_lines:
-    return
 
 def prompt_tape(all_lines):
     print("Search for times for specific tape? (y/n)")
     if input() == 'y':
         show_time_range(all_lines)
+    else:
+        exit(0)
+
 
 def species_per_tape(all_lines):
     tapes = {}
@@ -50,6 +53,22 @@ def species_per_tape(all_lines):
         else:
             tapes[image[len(image) - 1]] = 1
     return tapes
+
+
+def specific_species(all_lines):
+    choice = input("Enter species: ")
+    tapes = {}
+    for image in all_lines:
+        if image[0] == choice:
+            if image[len(image) - 1] not in tapes:
+                tapes[image[len(image) - 1]] = 1
+            else:
+                tapes[image[len(image) - 1]] += 1
+    print(tapes)
+    print('Results for '+choice)
+    for count in sorted(tapes, key=tapes.get):
+        print(str(count).ljust(15, ' '), str(tapes[count]).rjust(0))
+
 
 def start_and_end():
     start = int(input("Enter start seconds: "))
@@ -66,16 +85,19 @@ def start_and_end():
     e += str(end)+':00'
     print('Start: '+s, 'End: '+e, sep='\n')
 
+
 def print_tapes(occurences):
-    print('Results\n', 'Tape ID'.ljust(15, ' '), 'Count'.rjust(5), sep='')
+    print('Tape ID'.ljust(15, ' '), 'Count'.rjust(5), sep='')
     for count in sorted(occurences, key=occurences.get):
-        print(count.ljust(15, ' '), str(occurences[count]).rjust(0))
+        print(str(count).ljust(15, ' '), str(occurences[count]).rjust(0))
+
 
 def show_time_range(all_lines):
-    tapeId = input("Enter tape ID: ")
+    """Generate histogram to see species density throughout """
+    tape_id = input("Enter tape ID: ")
     timeStamp = {}
     for still in all_lines:
-        if still[len(still)-1] == tapeId:
+        if still[len(still)-1] == tape_id:
             t = still[3].split(':')
             seconds = int(t[2])+int(t[1])*60+int(t[0])*3600
             if seconds not in timeStamp:
@@ -84,7 +106,40 @@ def show_time_range(all_lines):
                 timeStamp[seconds] += 1
     plt.bar(timeStamp.keys(), timeStamp.values(), color='g')
     plt.show()
-    plt.savefig('histogram_'+tapeId+'.png', bbox_inches='tight')
+    plt.savefig('histogram_'+tape_id+'.png', bbox_inches='tight')
+
+
+def species_split_histogram(all_lines, video_id):
+    video = {}
+    timestamp = {}
+    for selection in all_lines:
+        if selection[len(selection)-1] == video_id:
+            if selection[0] not in video:
+                t = selection[3].split(':')
+                seconds = int(t[2]) + int(t[1]) * 60 + int(t[0]) * 3600
+                timestamp[seconds] = selection[3]
+                video[selection[0]] = [seconds]
+            else:
+                t = selection[3].split(':')
+                seconds = int(t[2]) + int(t[1]) * 60 + int(t[0]) * 3600
+                timestamp[seconds] = selection[3]
+                video[selection[0]].append(seconds)
+    all_species = []
+    for key in video.keys():
+        subset_timestamps = video[key]
+        all_species.append(subset_timestamps)
+    colors = ['#E69F00', '#D55E00', '#F0E442', '#000000']
+    plt.hist(all_species, color=colors, label=video.keys())
+
+    plt.legend(prop={'size': 12}, title=video_id)
+    plt.title(video_id)
+    plt.xlabel('Time')
+    plt.ylabel('Density')
+    if 'species_density_'+video_id+'.png' in os.listdir():
+        os.remove('species_density_'+video_id+'.png')
+    plt.savefig('species_density_'+video_id+'.png', bbox_inches='tight')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
