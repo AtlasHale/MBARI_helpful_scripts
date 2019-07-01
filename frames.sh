ddb3f1f91f9dbe51d982c61d025a88dc77331bdb
Connor Hale <37227138+AtlasHale@users.noreply.github.com>
2019-07-01 16:41:31 -0700

    add video directory as command line arg
    if you run ./frames.sh videos, your videos folder should look like this heirarchy 
    videos
    |____Echino
    |       |______D0008-02HD
    |       |______D0007-01HD
    |____Cystechinus
            |_______T1147-07
    etc.

--- frames.sh
+++ frames.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
-cd clips
-for f in *; do
-    echo $f
-    for video in *'.mp4'; do
-        mkdir "$video"_frames
-        ffmpeg -i $video -vf fps=.5 "$video"_frames/"$video"%04d.png -hide_banner;
-    done
+shopt -s nullglob
+cd $1
+mkdir all_frames
+for concept in *; do
+	echo $concept
+	cd $concept
+	for tape in *; do
+		echo $tape
+		cd $tape
+		for video in *.mov; do
+			temp=${video%.*}
+			ffmpeg -y -re -i $video -vf fps=10 ../../all_frames/"$concept"_"$temp"%03d.png -hide_banner;
+		done
+		cd ..
+	done
+	cd ..
 done
+
